e24a8152333916b899f362014def6b4f27a13e0f
Mark Brest <MarkAlanBrest@gmail.com>
2026-03-11 22:31:13 -0400

    P

--- app/course/page.tsx
+++ app/course/page.tsx
@@ -112,7 +112,7 @@ useEffect(() => {
       const folder = record.CourseName.replace(/\s+/g, "");
 
 // 3) Load correct course JSON
-const mod = await fetch(`/courses/${folder}/module.json`);
+const mod = await fetch(/${folder}/module.json);
       const data = await mod.json();
 
       setSlides(data.slides || []);
