66f6dc460a0dd3e1a642bda41ee66704a6ffe654
Mark Brest <MarkAlanBrest@gmail.com>
2026-03-13 22:24:18 -0400

    P

---
+++ app/api/login/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminIndex() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Admin Area</h1>
+      <a href="/admin/dashboard">Go to Dashboard</a>
+    </main>
+  );
+}
