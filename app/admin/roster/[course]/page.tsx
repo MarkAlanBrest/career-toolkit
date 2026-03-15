2b84decfd34768b6f5ef9d5f0185f0d402f983c2
Mark Brest <MarkAlanBrest@gmail.com>
2026-03-15 13:57:25 -0400

    P

--- app/admin/roster/[course]/page.tsx
+++ app/admin/roster/[course]/page.tsx
@@ -1,4 +1,7 @@
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
+
 
 import { prisma } from "@/lib/prisma";
 import RosterClient from "./RosterClient";
