b8c997cf200f13b46aac33bfcf9a90ae4e6f335c
Mark Brest <MarkAlanBrest@gmail.com>
2026-03-15 08:46:05 -0400

    P

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -24,14 +24,14 @@ export default function AdminDashboardPage() {
         {/* Log Out */}
 
 
-        <form action="/admin/logout" method="POST">
-          <button
-            type="submit"
-            className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 shadow-sm"
-          >
-            Log Out
-          </button>
-        </form>
+      <form action="/admin/logout" method="POST">
+  <button
+    type="submit"
+    className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 shadow-sm"
+  >
+    Log Out
+  </button>
+</form>
 
         
 
