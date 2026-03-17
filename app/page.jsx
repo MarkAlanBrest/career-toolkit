88eea0cc190d50f5d288acbb4b98ba3770a9ea53
Mark Brest <MarkAlanBrest@gmail.com>
2026-03-17 14:39:47 -0400

    P

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, textAlign: "center" }}>
+      <h1>Career Toolkit</h1>
+      <p>Site is working.</p>
+
+      <a href="/jobs">
+        <button style={{ padding: 12, fontSize: 16 }}>
+          Go to Job Board
+        </button>
+      </a>
+    </main>
+  );
+}
