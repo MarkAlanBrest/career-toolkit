c3d60edd8041493889c84bcbbd45811d14f5c91f
Mark Brest <MarkAlanBrest@gmail.com>
2026-03-17 14:25:38 -0400

    P

--- app/jobs/page.jsx
+++ app/jobs/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 
 export default function JobsPage() {
   const [tiles, setTiles] = useState([]);
